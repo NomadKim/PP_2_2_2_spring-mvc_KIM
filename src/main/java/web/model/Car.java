40eb6c5e506589709044815529049f9f29b56368
NomadKim <99548558+NomadKim@users.noreply.github.com>
2022-04-15 21:50:03 +0900

    Kim

--- src/main/java/web/model/Car.java
+++ src/main/java/web/model/Car.java
@@ -1,13 +1,27 @@
 package web.model;
 
 public class Car {
-    public String name;
-    public String model;
-    public Integer serial;
+    public String getName() {
+        return name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public Integer getSerial() {
+        return serial;
+    }
+
+    private String name;
+    private String model;
+    private Integer serial;
 
     public Car(String name, String model, Integer serial) {
         this.name = name;
         this.model = model;
         this.serial = serial;
     }
+
+
 }
